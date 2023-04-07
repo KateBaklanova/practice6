a7986509eb778f933b6cc22fa4a9afb1af8103ae
Kate <ggkate12321@gmail.com>
2023-04-07 18:53:02 +0300

    4

--- app/src/main/java/com/example/pr5/ExampleActivity.java
+++ app/src/main/java/com/example/pr5/ExampleActivity.java
@@ -9,10 +9,22 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Delete;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.Update;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class ExampleActivity extends AppCompatActivity {
 
@@ -36,9 +48,51 @@ public class ExampleActivity extends AppCompatActivity {
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 
+
+
+    @Entity
+    public class User {
+        @PrimaryKey
+        public int uid;
+        @ColumnInfo(name = "first_name")
+        public String firstName;
+        @ColumnInfo(name = "last_name")
+        public String lastName;
+    }
+
+    @Dao
+    public interface UserDao {
+        @Query("SELECT * FROM user")
+        List<User> getAll();
+        @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+        List<User> loadAllByIds(int[] userIds);
+        @Query("SELECT * FROM user WHERE first_name LIKE :first AND " + "last_name LIKE :last LIMIT 1")
+        User findByName(String first, String last);
+        @Insert
+        void insert(User user);
+        @Update
+        void update(User user);
+        @Insert
+        void insertMany(User... users);
+        @Delete
+        void delete(User user);
+    }
+
+    @Database(entities = {User.class}, version = 1)
+    public abstract class AppDatabase extends RoomDatabase {
+        public abstract UserDao userDao();
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").build();
+        UserDao userDao = db.userDao();
+        List<User> users = userDao.getAll();
+    }
 }
